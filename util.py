6f56de27f74e059d84064bf8a76bb0d9f3bde46d
lionael2006 <104011377+lionael2006@users.noreply.github.com>
2022-08-20 14:06:19 +0700

    Add files via upload

---
+++ util.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import plotly.graph_objs as go
+import plotly.express as px
+import yfinance as yf
+
+def get_ticker_data(ticker_symbol, data_period, data_interval):
+    ticker_data = yf.download(tickers=ticker_symbol, period=data_period, interval=data_interval)
+    if len(ticker_data) == 0:
+        st.write('Could not find the ticker data. Modify ticker symbol or reduce the Period value.')
+    else:
+        #Format the x-axis to skip dates with missing values
+        ticker_data.index = ticker_data.index.strftime("%d-%m-%Y %H:%M")
+    return ticker_data
+
+def plot_candle_chart(ticker_data):
+    candle_fig = go.Figure()
+    candle_fig.add_trace(
+        go.Candlestick(x=ticker_data.index,
+        open=ticker_data['Open'],
+        close=ticker_data['Close'],
+        low=ticker_data['Low'],
+        high=ticker_data['High'],
+        name='Market Data'
+        )
+    )
+    candle_fig.update_layout(
+        height=800,
+    )
+    st.write(candle_fig)
